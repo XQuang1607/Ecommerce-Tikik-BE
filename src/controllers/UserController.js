b9611501487fa012feb8a90e8e55a7cf633f1bdd
XQuang1607 <quangnxde160580@fpt.edu.vn>
2024-01-31 14:28:27 +0700

    update full profile

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -155,6 +155,20 @@ const refreshToken = async(req, res) => {
     }
 }
 
+const logoutUser = async(req, res) => {
+    try {
+        res.clearCookie('refresh_token')
+        return res.status(200).json({
+            status: 'OK',
+            message: 'Logout successfully'
+        })
+    } catch (e) {
+        return res.status(404).json({
+            message: e
+        })
+    }
+}
+
 module.exports = {
     createUser,
     loginUser,
@@ -162,5 +176,6 @@ module.exports = {
     deleteUser,
     getAllUser,
     getDetailsUser,
-    refreshToken
+    refreshToken,
+    logoutUser
 }
