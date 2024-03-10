16378736dadf0f75c2eb6f9a23d309f28daec8b1
XQuang1607 <quangnxde160580@fpt.edu.vn>
2024-03-10 08:31:55 +0700

    add-type-product-be

--- src/routes/ProductRouter.js
+++ src/routes/ProductRouter.js
@@ -10,4 +10,6 @@ router.delete('/delete/:id', authMiddleWare, ProductController.deleteProduct)
 router.get('/get-all', ProductController.getAllProduct)
 router.post('/delete-many', authMiddleWare, ProductController.deleteMany)
 
+router.get('/get-all-type', ProductController.getAllType)
+
 module.exports = router
